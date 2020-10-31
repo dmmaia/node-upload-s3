5bdc12a7cf79200f460f70139acf29d41bcaa0ad
dmmaia <david_mmaia@hotmail.com>
2020-10-31 13:33:32 -0300

    starting project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const port = 3333;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
+app.use(require('./routes'))
+
+app.listen(port,()=>{console.log(`Servidor rodando na porta ${port}`)})
